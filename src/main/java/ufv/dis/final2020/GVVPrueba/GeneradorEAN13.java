bfe12d2a86e434014e24f57a30ca86c913bf1022
Guillermo Ventas <willim.mail@gmail.com>
2020-06-30 12:44:26 +0200

    subida heroku

--- src/main/java/ufv/dis/final2020/GVVPrueba/GeneradorEAN13.java
+++ src/main/java/ufv/dis/final2020/GVVPrueba/GeneradorEAN13.java
@@ -1,6 +1,25 @@
 package ufv.dis.final2020.GVVPrueba;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+
 
 public class GeneradorEAN13 {
 	
+public static void generarEAN13(Producto p) throws IOException, WriterException {
+		
+        
+		EAN13Writer ean13writer = new EAN13Writer();
+		BitMatrix bitMatrix = ean13writer.encode(p.getEan13(), BarcodeFormat.EAN_13, 300, 100);
+		Path path = FileSystems.getDefault().getPath("productos/" + p.getEan13() + p.getNombre() + ".png");
+		MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+		
+	}
 }
